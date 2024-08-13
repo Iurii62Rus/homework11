455da0a5f7b0c0fd15a6925a3eebf089a6553efb
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-08-13 22:40:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import random
+def generate_password(n):
+    password = ""
+    if n <= 10:
+        i = 1
+        j = n - 1
+        while i <= j:
+            password += str(i)
+            if i != j:
+                password += str(j)
+            i += 1
+            j -= 1
+    else:
+        for i in range(1, n // 2 + 1):
+            password += str(i)
+            password += str(n - i)
+        if n % 2 != 0:
+            password += str(n // 2 + 1)
+    return password
 
-number = random.randint(3, 20)
-print(f'The given number: {number}')
-pairs = []
-for a in range(1, 21):
-    for b in range(1, 21):
-        if (a + b) % number == 0:
-            pairs.append((a, b))
-print('Matching pairs of numbers:')
-for pair in pairs:
-    print(pair)
 
+for n in range(3, 21):
+    result = generate_password(n)
+    print(f"{n} - {result}")
