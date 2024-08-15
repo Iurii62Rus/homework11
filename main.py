3b5805929e3704feb58a46915b7735df1a88fa49
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-08-15 19:11:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 def generate_password(n):
-    password = ""
-    if n <= 10:
-        i = 1
-        j = n - 1
-        while i <= j:
-            password += str(i)
-            if i != j:
-                password += str(j)
-            i += 1
-            j -= 1
-    else:
-        for i in range(1, n // 2 + 1):
-            password += str(i)
-            password += str(n - i)
-        if n % 2 != 0:
-            password += str(n // 2 + 1)
-    return password
+    result = ""
 
 
-for n in range(3, 21):
-    result = generate_password(n)
-    print(f"{n} - {result}")
+    for i in range(1, n):
+
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                result += f"{i}{j}"
+
+    return result
+
+
+n = 13
+password = generate_password(n)
+print(f"Сгенерированный пароль для n = {n}: {password}")
+
